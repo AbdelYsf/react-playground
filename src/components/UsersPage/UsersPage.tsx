24e752815d806cb52aa4f786b3669f8e58b38db6
abdellah.el-youssfi.alaoui <abdellah.el-youssfi.alaoui@octo.ma>
2022-09-10 12:07:53 +0100

    2-displaying list of users (enhancement)

--- src/components/UsersPage/UsersPage.tsx
+++ src/components/UsersPage/UsersPage.tsx
@@ -7,14 +7,12 @@ import {
   CardContent,
   CardMedia,
   Container,
-  CssBaseline,
   Grid,
   Skeleton,
   Stack,
   Typography,
 } from "@mui/material";
 import BasicAlert from "../Alert/Alert";
-import Box from "@mui/material/Box";
 
 interface AddressDetails {
   street: string;
@@ -35,13 +33,12 @@ export const UsersPage = () => {
 
   return (
     <>
-      <CssBaseline />
-      <Container maxWidth="sm">
+      <Container>
         {isLoading ? (
           <Stack spacing={1}>
             <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
             <Skeleton variant="circular" width={40} height={40} />
-            <Skeleton variant="rectangular" width={210} height={60} />
+            <Skeleton variant="rectangular" width={1110} height={60} />
             <Skeleton variant="rounded" width={210} height={60} />
           </Stack>
         ) : error ? (
@@ -51,54 +48,46 @@ export const UsersPage = () => {
             description={error.message}
           />
         ) : (
-          data.map((user: UserDetails, index: number) => {
-            return (
-              // breakpoints : xs , sm, md ,lg, xl: 12 unit
-              <Box sx={{ width: "100%" }}>
-                <Grid
-                  container
-                  spacing={4}
-                  direction="row"
-                  justifyContent="space-evenly"
-                  alignItems="center"
-                  rowSpacing={1}
-                  columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                >
-                  <Grid item xs={3} sm={6} md={4} lg={10}>
-                    <Card
-                      sx={{ maxWidth: 345 }}
-                      key={index}
-                      style={{ margin: 15 }}
-                    >
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="140"
-                          image="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-                          alt="green iguana"
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                            {user.name}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            {user.address.street} {user.address.suite}{" "}
-                            {user.address.city} <br />
-                            {user.phone}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                      <CardActions>
-                        <Button size="small" color="primary">
-                          Share
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {data.map((user: UserDetails, index: number) => {
+              return (
+                <Grid item xs={8} sm={4} md={3} lg={2}>
+                  <Card
+                    key={index}
+                    style={{ margin: 15, width: 170, height: 400 }}
+                  >
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+                        alt="green iguana"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {user.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          {user.address.street} {user.address.suite}{" "}
+                          {user.address.city} <br />
+                          {user.phone}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        Share
+                      </Button>
+                    </CardActions>
+                  </Card>
                 </Grid>
-              </Box>
-            );
-          })
+              );
+            })}
+          </Grid>
         )}
       </Container>
     </>
